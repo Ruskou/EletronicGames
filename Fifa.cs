bb58442e904b61b1f4f9afbe89471db474675810
Matheus Augusto Cavalheiro <70042716+Ruskou@users.noreply.github.com>
2020-09-13 18:56:42 -0300

    Add files via upload

---
+++ Fifa.cs
@@ -0,0 +1,18 @@
+using System;
+using Games.Model.Base;
+
+namespace Games.Model 
+{
+    public class Fifa : EletronicGames
+    {
+        public void Goals()
+        {
+            Console.WriteLine("Possibility to Score!");
+
+        }
+
+        
+       
+        
+    }
+}
