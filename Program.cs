f8999c49c9bea16b6e62f5e61cf4ffaa8615667f
Matheus Cavalheiro <matheuusz001@gmail.com>
2020-09-13 18:33:30 -0300

    EletronicGames

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Games.Model;
+using Games.Model.Base;
+
+namespace Games
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+        Console.WriteLine("Choose the game you want: ");
+        string choosenGame = Console.ReadLine();
+
+        EletronicGames eletronicgames = null;
+        if (choosenGame == "Fifa")
+        eletronicgames = new Fifa();
+        
+        eletronicgames.Name = "Fifa";
+        eletronicgames.Type = "Soccer";
+        eletronicgames.ParentalRating = 12;
+        eletronicgames.Memory = 10;
+        eletronicgames.Objective = "Win";
+                
+        
+        eletronicgames.Created();
+        eletronicgames.Acquired();
+        eletronicgames.Started();
+        eletronicgames.Played();
+        eletronicgames.TurnedOff();
+       
+            }
+
+            catch(Exception)
+            {
+                Console.WriteLine("There is no game!");
+
+                throw;
+            }
+        }
+    }
+    }
+
